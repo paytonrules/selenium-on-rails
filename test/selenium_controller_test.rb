83f32f6bceabb009b2c3ded8c419c5e13ee84e06
eric@8thlight.com <eric@8thlight.com@9274398c-e119-0410-8437-aa71ef7847aa>
2008-11-04 13:41:55 +0000

    Finally fixed an issue with test:acceptance that prevented getting real results.  How did that get there in the first place?
    git-svn-id: https://svn.openqa.org/svn/selenium-on-rails/selenium-on-rails@96 9274398c-e119-0410-8437-aa71ef7847aa

--- test/selenium_controller_test.rb
+++ test/selenium_controller_test.rb
@@ -10,6 +10,15 @@ class SeleniumControllerTest < Test::Unit::TestCase
     @request    = ActionController::TestRequest.new
     @response   = ActionController::TestResponse.new
     @result_dir = File.join(File.dirname(__FILE__), "..", "test_result")
+    
+    @suite = <<EOS
+<script>
+</script>
+<table>
+  <tr><td bgcolor="#ccffcc"><a href="/selenium/tests/foo.sel">Foo</a></td></tr>
+  <tr><td bgcolor="#ccffcc"><a href="/selenium/tests/bar.sel">Bar</a></td></tr>
+</table>
+EOS
   end
 
   def teardown
@@ -18,16 +27,8 @@ class SeleniumControllerTest < Test::Unit::TestCase
   
   def test_record_with_result
     @controller.config.configs["result_dir"] = @result_dir
-
-    suite = <<EOS
-<script>
-</script>
-<table>
-  <tr><td bgcolor="#ccffcc"><a href="/selenium/tests/foo.sel">Foo</a></td></tr>
-  <tr><td bgcolor="#ccffcc"><a href="/selenium/tests/bar.sel">Bar</a></td></tr>
-</table>
-EOS
-    post :record, :suite => suite, "testTable.1" => "<table></table>", "testTable.2" => "<table></table>"
+    
+    post :record, :suite => @suite, "testTable.1" => "<table></table>", "testTable.2" => "<table></table>"
     
     cur_result_dir = File.join(@result_dir, "default")
     assert File.directory?(cur_result_dir)
@@ -47,5 +48,19 @@ EOS
 </body></html>
 EOS
     assert_equal expected, File.read("#{cur_result_dir}/suite.html")
+  end
+  
+  def test_result_hash
+    post :record, :suite => @suite, "testTable.1" => "<table></table>", "testTable.2" => "<table></table>",
+                  :result => 'Failed', :numTestFailures => "906", :numTestPasses => "1079", :numCommandFailures => '1027', 
+                  :numCommandErrors => '57', :numCommandPasses => '3', :totalTime => "A long time"
+    
+    assert_equal 'Failed', assigns['result']['result']
+    assert_equal '906', assigns['result']['numTestFailures']
+    assert_equal '1079', assigns['result']['numTestPasses']
+    assert_equal '1027', assigns['result']['numCommandFailures']
+    assert_equal '57', assigns['result']['numCommandErrors']
+    assert_equal '3', assigns['result']['numCommandPasses']
+    assert_equal  'A long time', assigns['result']['totalTime']
   end
 end
