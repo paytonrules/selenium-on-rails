7d9e45fa9122eea349538820d8ccb63e7f2d88c4
Tapajós <tapajos@gmail.com>
2009-02-21 00:32:55 -0300

    Testing SeleniumOnRails::Paths.fixtures_path

--- test/paths_test.rb
+++ test/paths_test.rb
@@ -16,4 +16,14 @@ class SeleniumOnRails::PathsTest < Test::Unit::TestCase
     assert_equal "path", selenium_tests_path
   end
   
+  def test_fixtures_path_when_config_has_not_fixtures_path
+    SeleniumOnRailsConfig.expects(:get).with("fixtures_path").returns(nil)
+    assert_equal "#{RAILS_ROOT}/test/fixtures", fixtures_path
+  end
+  
+  def test_fixtures_path_when_config_has_fixtures_path
+    SeleniumOnRailsConfig.expects(:get).with("fixtures_path").returns("path").at_least_once
+    assert_equal "path", fixtures_path
+  end
+  
 end
